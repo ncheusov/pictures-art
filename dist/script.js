0a528bdad40d95d1599715fcdc4059c963a38527
Nikita Cheusov <ncheusov@inbox.ru>
2022-05-02 17:04:02 +0300

    some commit

--- dist/script.js
+++ dist/script.js
@@ -4457,12 +4457,18 @@ var accordeon = function accordeon(triggersSelector) {
   var btns = document.querySelectorAll(triggersSelector);
   btns.forEach(function (btn) {
     btn.addEventListener('click', function () {
-      this.classList.toggle('active-style');
-      this.nextElementSibling.classList.toggle('active-content');
-
-      if (this.classList.contains('active-style')) {
+      if (!this.classList.contains('active-style')) {
+        btns.forEach(function (item) {
+          item.classList.remove('active-style');
+          item.nextElementSibling.classList.remove('active-content');
+          item.nextElementSibling.style.maxHeight = '0px';
+        });
+        this.classList.add('active-style');
+        this.nextElementSibling.classList.add('active-content');
         this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
       } else {
+        this.classList.remove('active-style');
+        this.nextElementSibling.classList.remove('active-content');
         this.nextElementSibling.style.maxHeight = '0px';
       }
     });
--- src/js/modules/accordeon.js
+++ src/js/modules/accordeon.js
@@ -3,16 +3,23 @@ const accordeon = (triggersSelector) => {
 
     btns.forEach(btn => {
         btn.addEventListener('click', function() {
-            this.classList.toggle('active-style');
-            this.nextElementSibling.classList.toggle('active-content');
-
-            if (this.classList.contains('active-style')) {
+            if (!this.classList.contains('active-style')) {
+                btns.forEach(item => {
+                    item.classList.remove('active-style');
+                    item.nextElementSibling.classList.remove('active-content');
+                    item.nextElementSibling.style.maxHeight = '0px';
+                });
+                this.classList.add('active-style');
+                this.nextElementSibling.classList.add('active-content');
                 this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
             } else {
+                this.classList.remove('active-style');
+                this.nextElementSibling.classList.remove('active-content');
                 this.nextElementSibling.style.maxHeight = '0px';
             }
         });
     });
-};
+}
+
 
 export default accordeon;
